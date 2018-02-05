9ad58b01f9f6f3d3d6d603cbd432540ed7e47611
Eugene Krasotkin <still-neon@tut.by>
2018-02-05 13:43:51 +0300

    update

--- src/main/java/webapplication/LoginServlet.java
+++ src/main/java/webapplication/LoginServlet.java
@@ -1,6 +1,8 @@
 package webapplication;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,9 +12,16 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
         try {
             if (UserService.checkCredentials(request.getParameter("login"), request.getParameter("password"))) {
                 getServletContext().getRequestDispatcher("/welcome_page.jsp").forward(request, response);
+            }
+            else {
+                out.print("Sorry username or password error");
+                RequestDispatcher rd=request.getRequestDispatcher("login_page.html");
+                rd.include(request,response);
             }
         } catch (Exception e) {
             e.printStackTrace();
--- target/MyWebApp2.0/welcome_page.jsp
+++ target/MyWebApp2.0/welcome_page.jsp
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Welcome page</title>
     <p>Welcome dude! <%=new Date(System.currentTimeMillis())%></p>
 </head>
 <body>
